87e69606436728b6404434c8da8e9de2d4c82ca1
Ryan Dejaegher <dejaegher.ryan@gmail.com>
2020-04-12 22:23:44 -0400

    update

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,12 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
+
 module.exports = {
     plugins: [
         require('tailwindcss'),
         require('autoprefixer'),
+        purgecss({
+            content: ['themes/novela/layouts/**/*.html'],
+            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+            })
     ]
 }
