5a808015fede5130802757bbfaeeccbcc1b61de2
ryandejaegher <dejaegher.ryan@gmail.com>
2020-06-05 06:30:31 -0400

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,19 @@
 module.exports = {
   theme: {
+    fontFamily: {
+      body: ['futura'],
+      display: ['']
+    },
     extend: {
+      colors: {
+        primary: 'hotpink',
+        lightPrimary: 'var(--primary-light)'
+        // lightSecondary
+        // lightAccent
+        // darkPrimary
+        // darkSecondary
+        // darkAccent
+      },
       padding: {
         '16/9': '56.25%',
         '3/2': '66.67%',
